a6e1021e72a6237866bb1c22952bb37a9aeeede5
antoinechalifour <antoine.chalifour@gmail.com>
2015-03-15 16:21:39 +0100

    Modification scroll maps, suppression menu mobile

--- www/about/about.directives.js
+++ www/about/about.directives.js
@@ -9,4 +9,18 @@
       templateUrl: 'about/about.template.html'
     };
   })
+
+  .directive('ascMaps', function(){
+    return function(scope, element, attrs){
+      console.log('totot');
+      var mapOptions = {
+        center: { lat: 46.6071, lng: -1.8548},
+        zoom: 16,
+        scrollwheel: false
+      };
+      element.css('width', attrs.width);
+      element.css('height', attrs.height);
+      var map = new google.maps.Map(document.getElementById(attrs.id), mapOptions);
+    };
+  })
 })();
