475c5990f6df04b177bc602b7082e1ee85277cd2
antoinechalifour <antoine.chalifour@gmail.com>
2015-03-16 20:01:13 +0100

    Suppression de la description des sponsors

--- data/sponsors.js
+++ data/sponsors.js
@@ -5,32 +5,24 @@
 // Pour la mettre à jour il suffit d'ajout / supprimer / modifier l'array suivant.
 // Pour chaque sponsors il faut fournir :
 // {
-//    name: //le nom
-//    logo: //l'image
+//    name: // le nom
+//    logo: // l'image avec ratio 4/3 (genre 800/600)
 //    url: //un lien vers leur site
 // }
 module.exports = [{
     name: 'La Gachère',
     logo: 'assets/img/gachere/logo.png',
-    description: 'Hôtes depuis <année>.',
-    img: '',
     url: 'http://www.camping-gachere.com/'
 }, {
     name: 'Atlantic Lezard',
     logo: 'assets/img/al/logo.png',
-    description: 'Cours de surf, depuis <année>',
-    img: '',
     url: 'http://lezardsurfschool.com/'
 }, {
     name: 'BNP Paribas',
     logo: 'assets/img/bnp/logo.png',
-    description: 'Partenaires depuis <année>',
-    img: '',
     url: 'https://www.secure.bnpparibas.net/'
 }, {
     name: 'Red Bull',
     logo: 'assets/img/rb/logo.png',
-    description: 'Partenaires depuis <année>',
-    img: '',
     url: 'http://www.redbull.com/fr/fr'
 }];
